b4a37f6833224466588233c01d0cad848451404b
katya <katya@beregov.com>
2025-06-22 13:53:38 +0300

    Добавила файлы query1.sql - проверяет отображается ли созданный заказ в базе и query2.sql - запрос выводит все трекеры заказов и их статус; Добавила скриншот удачно пройденного автотеста; Закомментировала автотест

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://692bd00c-906d-46a0-9452-7d587079209c.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://45888e5d-3448-4b87-bb4a-dcc371209406.serverhub.praktikum-services.ru"
 
 CREATE_ORDER_PATH = "/api/v1/orders"
 
--- order_test.py
+++ order_test.py
@@ -1,6 +1,6 @@
 import sender_stand_request
 
-
+# Берегова Екатерина, 31 когорта, финальный проект, Инженер по тестированию расширенный
 def test_create_order():
     order_body = {
         "firstName": "Naruto",
@@ -15,18 +15,15 @@ def test_create_order():
             "BLACK"
         ]
     }
-
     order_response = sender_stand_request.post_new_client_order(order_body)
 
     # Проверка, что код ответа равен 201
     assert order_response.status_code == 201, f"Expected status code 201, but got {order_response.status_code}"
 
     track = order_response.json().get("track")
-
     track_params = {
         "t": track
     }
-
     track_response = sender_stand_request.get_client_order_track(track_params)
 
     assert track_response.status_code == 200, f"Expected status code 200, but got {track_response.status_code}"
